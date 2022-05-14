cd0793cbf07a7db1a19abb3d3d6fc948498ca569
michalovsky <michalovskyyy@gmail.com>
2022-05-14 20:03:44 +0200

    add product node js lambda function

--- lib/e-commerce-app-stack.ts
+++ lib/e-commerce-app-stack.ts
@@ -1,6 +1,9 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 
 export class ECommerceAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -15,5 +18,19 @@ export class ECommerceAppStack extends Stack {
       removalPolicy: RemovalPolicy.DESTROY,
       billingMode: BillingMode.PAY_PER_REQUEST,
     });
+
+    const productFunction = new NodejsFunction(this, 'productLambdaFunction', {
+      bundling: {
+        externalModules: ['aws-sdk'],
+      },
+      environment: {
+        PRIMARY_KEY: 'id',
+        DB_TABLE_NAME: productTable.tableName,
+      },
+      runtime: Runtime.NODEJS_16_X,
+      entry: join(__dirname, '/../src/product/index.js'),
+    });
+
+    productTable.grantReadWriteData(productFunction);
   }
 }
