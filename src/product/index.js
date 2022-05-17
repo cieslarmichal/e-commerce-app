24977ecfd2cd47064fd6c0ad0414e076f830bb27
michalovsky <michalovskyyy@gmail.com>
2022-05-17 19:44:34 +0200

    add crud operations on product

--- src/product/index.js
+++ src/product/index.js
@@ -1,23 +1,65 @@
-import { GetItemCommand, ScanCommand } from '@aws-sdk/client-dynamodb';
+import {
+  GetItemCommand,
+  ScanCommand,
+  PutItemCommand,
+  DeleteItemCommand,
+  UpdateItemCommand,
+  QueryCommand,
+} from '@aws-sdk/client-dynamodb';
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 import { dynamoDbClient } from './dynamoDbClient';
+import { v4 as uuid4 } from 'uuid';
 
 exports.handler = async function (event) {
   console.log(event);
 
   switch (event.httpMethod) {
     case 'GET': {
-      if (event.pathParameters !== null) {
-        body = await getProduct(event.pathParameters.id);
+      if (event.queryStringParameters !== null) {
+        const category = event.queryStringParameters.category;
+
+        body = await getProductsByCategory(category);
+      } else if (event.pathParameters !== null) {
+        const productId = event.pathParameters.id;
+
+        body = await getProduct(productId);
       } else {
         body = await getAllProducts();
       }
+
+      break;
+    }
+    case 'POST': {
+      const productProperties = JSON.parse(event.body);
+
+      body = await createProduct(productProperties);
+
+      break;
+    }
+    case 'PUT': {
+      const productId = event.pathParameters.id;
+
+      const productProperties = JSON.parse(event.body);
+
+      body = await updateProduct(productId, productProperties);
+
+      break;
+    }
+    case 'DELETE': {
+      const productId = event.pathParameters.id;
+
+      body = await deleteProduct(productId);
+
+      break;
+    }
+    default: {
+      throw new Error(`Unsupported route: "${event.httpMethod}"`);
     }
   }
 
   return {
     statusCode: 200,
-    body: `hello, hitting ${event.path}`,
+    body,
   };
 };
 
@@ -54,6 +96,112 @@ const getAllProducts = async () => {
     console.log(Items);
 
     return Items ? Items.map((item) => unmarshall(item)) : {};
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const getProductsByCategory = async (category) => {
+  console.log('getProductsByCategory', category);
+
+  try {
+    const { Items } = await dynamoDbClient.send(
+      new QueryCommand({
+        TableName: process.env.DB_TABLE_NAME,
+        FilterExpression: 'contains (category, :category)',
+        ExpressionAttributeValues: {
+          ':category': { S: category },
+        },
+      }),
+    );
+
+    console.log(Items);
+
+    return Items ? Items.map((item) => unmarshall(item)) : {};
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const createProduct = async (productProperties) => {
+  console.log('createProduct', productProperties);
+
+  try {
+    const productId = uuid4();
+
+    productProperties.id = productId;
+
+    const result = await dynamoDbClient.send(
+      new PutItemCommand({
+        TableName: process.env.DB_TABLE_NAME,
+        Item: marshall(productProperties || {}),
+      }),
+    );
+
+    console.log(result);
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const deleteProduct = async (productId) => {
+  console.log('deleteProduct', productId);
+
+  try {
+    const result = await dynamoDbClient.send(
+      new DeleteItemCommand({
+        TableName: process.env.DB_TABLE_NAME,
+        Key: marshall({ id: productId }),
+      }),
+    );
+
+    console.log('Success, item deleted', result);
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const updateProduct = async (productId, productProperties) => {
+  console.log('updateProduct', productId, productProperties);
+
+  try {
+    const productPropertiesKeys = Object.keys(productProperties);
+
+    const result = await dynamoDbClient.send(
+      new UpdateItemCommand({
+        TableName: process.env.DB_TABLE_NAME,
+        Key: marshall({ id: productId }),
+        UpdateExpression: `SET ${productPropertiesKeys.map((_, index) => `#key${index} = :value${index}`).join(', ')}`,
+        ExpressionAttributeNames: productPropertiesKeys.reduce(
+          (acc, key, index) => ({
+            ...acc,
+            [`#key${index}`]: key,
+          }),
+          {},
+        ),
+        ExpressionAttributeValues: marshall(
+          productPropertiesKeys.reduce(
+            (acc, key, index) => ({
+              ...acc,
+              [`:value${index}`]: productProperties[key],
+            }),
+            {},
+          ),
+        ),
+      }),
+    );
+
+    console.log('Success, item updated', result);
+
+    return result;
   } catch (error) {
     console.error(error);
     throw error;
