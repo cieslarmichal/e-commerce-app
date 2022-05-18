d2d903b844fbe837cfe65b65684d170e68ff09b6
michalovsky <michalovskyyy@gmail.com>
2022-05-18 14:37:43 +0200

    add orders lambda function

--- lib/microservices.ts
+++ lib/microservices.ts
@@ -7,17 +7,22 @@ import { join } from 'path';
 export interface MicroservicesProperties {
   readonly productsTable: ITable;
   readonly basketsTable: ITable;
+  readonly ordersTable: ITable;
 }
 
 export class Microservices extends Construct {
   public readonly productsMicroservice: NodejsFunction;
   public readonly basketsMicroservice: NodejsFunction;
+  public readonly ordersMicroservice: NodejsFunction;
 
   constructor(scope: Construct, id: string, properties: MicroservicesProperties) {
     super(scope, id);
 
     this.productsMicroservice = this.createProductsFunction(properties.productsTable);
+
     this.basketsMicroservice = this.createBasketsFunction(properties.basketsTable);
+
+    this.ordersMicroservice = this.createOrdersFunction(properties.ordersTable);
   }
 
   createProductsFunction(productsTable: ITable) {
@@ -54,5 +59,24 @@ export class Microservices extends Construct {
     basketsTable.grantReadWriteData(basketsFunction);
 
     return basketsFunction;
+  }
+
+  createOrdersFunction(ordersTable: ITable) {
+    const ordersFunction = new NodejsFunction(this, 'OrdersLambdaFunction', {
+      bundling: {
+        externalModules: ['aws-sdk'],
+      },
+      environment: {
+        PRIMARY_KEY: 'email',
+        SORT_KEY: 'orderDate',
+        DB_TABLE_NAME: ordersTable.tableName,
+      },
+      runtime: Runtime.NODEJS_16_X,
+      entry: join(__dirname, '/../src/order/index.js'),
+    });
+
+    ordersTable.grantReadWriteData(ordersFunction);
+
+    return ordersFunction;
   }
 }
