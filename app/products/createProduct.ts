fbec7abb7783daf455ff4bd620a7edb54415d554
michalovsky <michalovskyyy@gmail.com>
2022-05-21 11:33:15 +0200

    fix create product function

--- app/products/createProduct.ts
+++ app/products/createProduct.ts
@@ -3,14 +3,19 @@ import { marshall } from '@aws-sdk/util-dynamodb';
 import { APIGatewayEvent, ProxyResult } from 'aws-lambda';
 import { commonMiddleware, dynamoDbClient } from './shared';
 import { StatusCodes } from 'http-status-codes';
+import { v4 as uuid4 } from 'uuid';
 
 async function createProduct(event: APIGatewayEvent): Promise<ProxyResult> {
-  const basketProperties = JSON.parse(event.body as string);
+  const productProperties = JSON.parse(event.body as string);
+
+  const productId = uuid4();
+
+  productProperties.id = productId;
 
   const result = await dynamoDbClient.send(
     new PutItemCommand({
       TableName: process.env.DB_TABLE_NAME,
-      Item: marshall(basketProperties || {}),
+      Item: marshall(productProperties || {}),
     }),
   );
 
