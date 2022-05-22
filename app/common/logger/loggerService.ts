9990ce64a5a9eb1379f2b8bc2338f1d6ff74866c
michalovsky <michalovskyyy@gmail.com>
2022-05-22 11:20:52 +0200

    add logger

---
+++ app/common/logger/loggerService.ts
@@ -0,0 +1,37 @@
+import Logger, { LogLevel } from 'bunyan';
+import bunyan from 'bunyan';
+import { LogContext } from './types';
+
+export class LoggerService {
+  private readonly instance: Logger;
+
+  public constructor() {
+    const logLevel = process.env.LOG_LEVEL as LogLevel;
+
+    this.instance = bunyan.createLogger({ name: 'loggerService', level: logLevel });
+  }
+
+  public fatal(message: string, context?: LogContext): void {
+    this.instance.fatal({ context }, message);
+  }
+
+  public error(message: string, context?: LogContext): void {
+    this.instance.error({ context }, message);
+  }
+
+  public warn(message: string, context?: LogContext): void {
+    this.instance.warn({ context }, message);
+  }
+
+  public info(message: string, context?: LogContext): void {
+    this.instance.info({ context }, message);
+  }
+
+  public debug(message: string, context?: LogContext): void {
+    this.instance.debug({ context }, message);
+  }
+
+  public log(message: string, context?: LogContext): void {
+    this.instance.info({ context }, message);
+  }
+}
