c4ee30ed97b1679d9bba5d72b4ce20aa0de224a4
michalovsky <michalovskyyy@gmail.com>
2022-05-23 20:06:18 +0200

    improve common middleware in baskets

--- app/baskets/shared/commonMiddleware.ts
+++ app/baskets/shared/commonMiddleware.ts
@@ -1,5 +1,17 @@
 import middy from '@middy/core';
-import httpErrorHandler from '@middy/http-error-handler';
 import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpSecurityHeaders from '@middy/http-security-headers';
+import JSONErrorHandlerMiddleware from 'middy-middleware-json-error-handler';
 
-export const commonMiddleware = (handler: any) => middy(handler).use([httpEventNormalizer(), httpErrorHandler()]);
+export function LoggerMiddleware<T, R>(): middy.MiddlewareObj<T, R> {
+  return {
+    onError: async (handler): Promise<void> => {
+      const error = handler.error;
+
+      console.log(error);
+    },
+  };
+}
+
+export const commonMiddleware = (handler: any) =>
+  middy(handler).use([httpSecurityHeaders(), httpEventNormalizer(), LoggerMiddleware(), JSONErrorHandlerMiddleware()]);
