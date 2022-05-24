f61ea4e676a7977c539fa8ff29b6a815647f0df7
michalovsky <michalovskyyy@gmail.com>
2022-05-24 16:01:37 +0200

    add delete product from basket lambda infra

--- infra/lambdaFunctions/baskets/index.ts
+++ infra/lambdaFunctions/baskets/index.ts
@@ -4,3 +4,4 @@ export * from './getBasketsLambda';
 export * from './checkoutBasketLambda';
 export * from './deleteBasketLambda';
 export * from './addProductToBasketLambda';
+export * from './deleteProductFromBasketLambda';
