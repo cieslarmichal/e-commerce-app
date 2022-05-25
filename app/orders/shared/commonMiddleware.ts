52beb046f663fdfeb3277c07a887f28a83243951
michalovsky <michalovskyyy@gmail.com>
2022-05-25 16:11:53 +0200

    add common middleware and dynamodb document client to orders

--- app/orders/shared/commonMiddleware.ts
+++ app/orders/shared/commonMiddleware.ts
@@ -1,5 +1,31 @@
-import middy from '@middy/core';
-import httpErrorHandler from '@middy/http-error-handler';
 import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpSecurityHeaders from '@middy/http-security-headers';
+import { ValidationError } from '../../common';
+import createError from 'http-errors';
+import middy from '@middy/core';
+import httpHeaderNormalizer from '@middy/http-header-normalizer';
 
-export const commonMiddleware = (handler: any) => middy(handler).use([httpEventNormalizer(), httpErrorHandler()]);
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+const httpErrorMiddleware = (): middy.MiddlewareObj<APIGatewayProxyEvent, APIGatewayProxyResult> => {
+  const onError: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult> = async (request): Promise<void> => {
+    let httpError;
+
+    console.log(request.error);
+
+    if (request.error instanceof ValidationError) {
+      httpError = new createError.BadRequest(request.error.message);
+    } else {
+      httpError = new createError.InternalServerError(JSON.stringify(request.error));
+    }
+
+    request.response = { body: JSON.stringify({ message: httpError?.message! }), statusCode: httpError?.statusCode! };
+  };
+
+  return {
+    onError,
+  };
+};
+
+export const commonMiddleware = (handler: any) =>
+  middy(handler).use([httpHeaderNormalizer(), httpSecurityHeaders(), httpEventNormalizer(), httpErrorMiddleware()]);
--- app/orders/shared/dynamoDbClient.ts
+++
@@ -1,5 +0,0 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-
-const dynamoDbClient = new DynamoDBClient({});
-
-export { dynamoDbClient };
